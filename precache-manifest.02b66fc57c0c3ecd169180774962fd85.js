e7d72f26c18a121ab80a5514f9c0e6dc78035f07
Daniel Kornas <harvi_pl@wp.pl>
2019-01-10 12:32:02 +0100

    Updates

---
+++ precache-manifest.02b66fc57c0c3ecd169180774962fd85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b61658b2664bfbbcf66f",
+    "url": "/shopping-list/static/js/runtime~main.b61658b2.js"
+  },
+  {
+    "revision": "285e01ad46465952611e",
+    "url": "/shopping-list/static/js/main.285e01ad.chunk.js"
+  },
+  {
+    "revision": "dd01588ae8336c81d14a",
+    "url": "/shopping-list/static/js/1.dd01588a.chunk.js"
+  },
+  {
+    "revision": "285e01ad46465952611e",
+    "url": "/shopping-list/static/css/main.3af34a8c.chunk.css"
+  },
+  {
+    "revision": "dd01588ae8336c81d14a",
+    "url": "/shopping-list/static/css/1.2b40e12b.chunk.css"
+  },
+  {
+    "revision": "7f29225c4a1b6c368472dcaa461087b6",
+    "url": "/shopping-list/index.html"
+  }
+];
