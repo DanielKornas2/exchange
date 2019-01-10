5f68ef675f04049f331751c0a865cbabfe15ce4f
Daniel Kornas <harvi_pl@wp.pl>
2019-01-10 12:35:02 +0100

    Updates

---
+++ precache-manifest.fe4a8a3fe50e485ac1512b5eeec7395d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a4069e52baa351a1dd1",
+    "url": "/exchange/static/js/runtime~main.0a4069e5.js"
+  },
+  {
+    "revision": "24eb726506b9f3801eef",
+    "url": "/exchange/static/js/main.24eb7265.chunk.js"
+  },
+  {
+    "revision": "dd01588ae8336c81d14a",
+    "url": "/exchange/static/js/1.dd01588a.chunk.js"
+  },
+  {
+    "revision": "24eb726506b9f3801eef",
+    "url": "/exchange/static/css/main.3af34a8c.chunk.css"
+  },
+  {
+    "revision": "dd01588ae8336c81d14a",
+    "url": "/exchange/static/css/1.2b40e12b.chunk.css"
+  },
+  {
+    "revision": "c0f604fd67224406ea5798909fe914ea",
+    "url": "/exchange/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/shopping-list/precache-manifest.02b66fc57c0c3ecd169180774962fd85.js"
+  "/exchange/precache-manifest.fe4a8a3fe50e485ac1512b5eeec7395d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/shopping-list/index.html", {
+workbox.routing.registerNavigationRoute("/exchange/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
